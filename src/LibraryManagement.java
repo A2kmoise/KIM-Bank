e820b8ded8f88a40b5a878906002eace01e0ce92
A2kmoise <abayomoise950@gmail.com>
2025-10-09 21:36:58 +0200

    -{ * * }- Library-console main1.0

---
+++ src/LibraryManagement.java
@@ -0,0 +1,6 @@
+
+public class LibraryManagement{
+    public static void main(String[] args){
+        System.out.println("\t\t\t\t\t\t \t\t\t\t\t\t\t\t=====================================+ Welcome to URS library +========================================");
+    }
+}
