6641cb115b2b7d044e72692de65967caf8fdb3da
A2kmoise <abayomoise950@gmail.com>
2025-10-09 22:05:31 +0200

    -{ * * }- main1.1 role -fix

--- src/User.java
+++ src/User.java
@@ -1,2 +1,33 @@
-public class User {
+import java.util.Date;
+
+abstract  class User {
+int id;
+String name;
+String role;
+public User(){}
+    public User(int id, String name, String role){
+        this.id = id;
+        this.name = name;
+        this.role = role;
+    }
+abstract void getRole();
+
+}
+
+class Admin extends User {
+@Override
+    void getRole(){
+    System.out.println("I am admin");
+}
+}
+
+class Student extends User {
+String department;
+Date createAt;
+@Override
+    void getRole(){
+    System.out.println("I am student");
+}
+
+
 }
