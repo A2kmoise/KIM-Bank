6f76fa5143f5bb7fb479de02a580725d3c3d7fb4
A2kmoise <abayomoise950@gmail.com>
2025-10-10 09:33:03 +0200

    -{ * * }- User handled

--- src/User.java
+++ src/User.java
@@ -1,33 +1,56 @@
+import javax.management.relation.Role;
 import java.util.Date;
 
 abstract  class User {
 int id;
 String name;
 String role;
+
 public User(){}
+
     public User(int id, String name, String role){
         this.id = id;
         this.name = name;
         this.role = role;
     }
-abstract void getRole();
+abstract String getRole();
+
+
+@Override
+public String toString(){
+    return ( "ID: "+ id + " \tNAME: " + name + " \tROLE: " +role);
+}
 
 }
 
 class Admin extends User {
-@Override
-    void getRole(){
-    System.out.println("I am admin");
+
+public  Admin(int id, String name){
+    super(id, name, "ADMIN");
 }
+    @Override
+    String getRole(){
+        return(this.role);
+    }
 }
 
 class Student extends User {
 String department;
 Date createAt;
-@Override
-    void getRole(){
-    System.out.println("I am student");
+
+public Student (int id, String name,String role, String department, Date createAt) {
+    super(id, name, (role != "ADMIN") ? "STUDENT": role);
+    this.department = department;
+    this.createAt = createAt;
 }
 
+@Override
+    String getRole(){
+    return(this.role);
+}
 
+@Override
+    public String toString(){
+    return ("ID: " + id + "\tNAME: " + name +  "\tDEPARTMENT: "+ department + "\tCREATED AT: " + createAt +"\tROLE: "+ role);
+}
 }
